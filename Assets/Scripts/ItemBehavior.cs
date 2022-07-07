1412b701f591753a9f2d457be0efdabdfc080003
Avetisovave <avetisov-v@mail.ru>
2022-07-07 18:28:08 +0300

    add Jump , Bullet , OnGUI

--- Assets/Scripts/ItemBehavior.cs
+++ Assets/Scripts/ItemBehavior.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected");
+
+            gameManager.Items += 1;
         }
     }
 }
