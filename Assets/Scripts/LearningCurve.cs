a6d4b4a5f265b1ccab1642caa6cc3cf2074bdde6
Avetisovave <avetisov-v@mail.ru>
2022-07-07 13:34:50 +0300

    add GamePlace Player and move

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LearningCurve : MonoBehaviour
+{
+    public Transform camTransform;
+    public GameObject directionLight;
+    public Transform lightTransform;
+    private void Start()
+    {
+       // directionLight = GameObject.Find("Directional Light");
+        lightTransform = directionLight.GetComponent<Transform>();
+        Debug.Log(lightTransform.localPosition);
+        camTransform = this.GetComponent<Transform>();
+        Debug.Log(camTransform.localPosition);
+        Character hero = new Character();
+        Character hero2 = hero;
+        hero2.name = "Vlad ebaniy";
+        
+     //   hero.PrintStatsInfo();
+       // hero2.PrintStatsInfo();
+       // hero2.Reset();
+       // hero2.PrintStatsInfo();
+        //Weapon huntingBow = new Weapon("HuntingBow", 105);
+       // huntingBow.PrintWeaponstats();
+       Weapon hutingBow = new Weapon("HuntingBow", 105);
+        Paladin knight = new Paladin("sir Arthur" , hutingBow );
+        knight.PrintStatsInfo();
+        hutingBow.PrintWeaponstats();
+       // Character heroine = new Character("Aghata");
+       //  heroine.PrintStatsInfo();
+       
+
+
+
+
+    }
+}
