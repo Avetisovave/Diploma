47b2343acf35f4f61e0528d9f21ef30ff0614001
Avetisovave <avetisov-v@mail.ru>
2022-08-10 19:39:10 +0300

    add Menu UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    private int _indexSceneGame = 1;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_indexSceneGame);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
